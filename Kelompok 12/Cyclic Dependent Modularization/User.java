5c553932635fc1a9e21e1757d9c89778611b4fbc
San <istamarahsan@gmail.com>
2023-06-07 12:28:09 +0700

    sesi 23-24

---
+++ Kelompok 12/Cyclic Dependent Modularization/User.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+    private String name;
+    private List<Role> roles;
+
+    public User(String name) {
+        this.name = name;
+        roles = new ArrayList<>();
+    }
+
+    public void addRole(Role role) {
+        roles.add(role);
+    }
+
+    public List<Role> getRoles() {
+        return roles;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ Kelompok 12/Deficient Encapsulation/before/Rectangle.java
@@ -0,0 +1,14 @@
+package before;
+
+public class Rectangle {
+    public int width;
+    public int height;
+
+    public int calculateArea() {
+        return width * height;
+    }
+
+    public int calculatePerimeter() {
+        return 2 * (width + height);
+    }
+}
